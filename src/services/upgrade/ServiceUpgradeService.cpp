377377310aa100a5c2e0764a836fbd4de60e82a1
wangzhiwei7497 <wangzhiwei7497@mindlinker.com>
2024-09-23 15:49:58 +0800

    refactor: 采用boost库优化命令执行

--- src/services/upgrade/ServiceUpgradeService.cpp
+++ src/services/upgrade/ServiceUpgradeService.cpp
@@ -1,16 +1,26 @@
+#include <boost/process.hpp>
+
 #include "services/upgrade/ServiceUpgradeService.h"
+
+namespace bp = boost::process;
 
 namespace ServiceUpgradeService {
 void Upgrade(const std::string& file, const std::string& file_name) {
-    // 使用tar命令进行解压
-    std::string tar = "tar -zxvf " + file + " -C /usr/bin/galaxy-series";
-    system(tar.c_str());
-    // 将 /usr/bin/galaxy-series 下的最新的文件夹 创建动态链接到 /usr/bin/galaxy-series/galaxy
-    std::string link = "rm -rf /usr/bin/galaxy-series/galaxy && ln -s /usr/bin/galaxy-series/" + file_name +
-                       " /usr/bin/galaxy-series/galaxy";
-    system(link.c_str());
-    // 重启应用
-    std::string restart = "/etc/init.d/S99galaxy restart";
-    system(restart.c_str());
+    try {
+        // 使用tar命令进行解压
+        std::string tar = "tar -zxvf " + file + " -C /usr/bin/galaxy-series";
+        bp::system(tar);
+
+        // 将 /usr/bin/galaxy-series 下的最新的文件夹 创建动态链接到 /usr/bin/galaxy-series/galaxy
+        std::string link = "rm -rf /usr/bin/galaxy-series/galaxy && ln -s /usr/bin/galaxy-series/" + file_name +
+                           " /usr/bin/galaxy-series/galaxy";
+        bp::system(link);
+
+        // 重启应用
+        std::string restart = "/etc/init.d/S99galaxy restart";
+        bp::system(restart);
+    } catch (const std::exception& e) {
+        CROW_LOG_ERROR << "Error during upgrade: " << e.what();
+    }
 }
 }  // namespace ServiceUpgradeService
