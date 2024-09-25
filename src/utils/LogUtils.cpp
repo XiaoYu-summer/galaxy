93938348205963804491ad6eca5ebbd6e4aab63e
wangzhiwei7497 <wangzhiwei7497@mindlinker.com>
2024-09-25 20:52:04 +0800

    refactor: log path

--- src/utils/LogUtils.cpp
+++ src/utils/LogUtils.cpp
@@ -11,10 +11,13 @@ FileLogHandler::FileLogHandler() {
         std::cout << "Current path: " << current_path << std::endl;
 
         // 拼接日志文件路径
-        boost::filesystem::path log_path = current_path / "logs" / "galaxy-server.log";
+        boost::filesystem::path log_path = current_path.parent_path() / "logs" / "galaxy-server.log";
         std::cout << "Log path: " << log_path << std::endl;
         m_logger = spdlog::rotating_logger_mt("galaxy_logger", log_path.string(), max_size, max_files);
         m_logger->set_pattern("%Y-%m-%d %H:%M:%S[%l] %v");
+        m_logger->info("Log init success!\n current path: " + current_path.string() +
+                       "\n log path: " + log_path.string());
+        m_logger->flush();
     } catch (const spdlog::spdlog_ex& ex) {
         std::cout << "Log init failed: " << ex.what() << std::endl;
     }
