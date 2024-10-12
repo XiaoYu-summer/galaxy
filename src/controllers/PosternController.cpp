c3410d76b964f5c6dbf4426ac297a50de78acbf8
wangzhiwei7497 <wangzhiwei7497@mindlinker.com>
2024-10-12 18:00:06 +0800

    refactor: 添加postern前缀

--- src/controllers/PosternController.cpp
+++ src/controllers/PosternController.cpp
@@ -4,20 +4,21 @@
 #include "utils/ResUtils.h"
 
 void PosternController::InitRoutes(CrowApp& app) {
-    CROW_ROUTE(app, "/service/restart").methods("POST"_method)([&](const crow::request& req, crow::response& res) {
-        auto body = crow::json::load(req.body);
-        if (!body || !body.has("pwd") || body["pwd"].s() != "CVTEMH") {
-            return FailResponse(res, ErrorCode::PARAMS_ERROR, "params error");
-        }
+    CROW_ROUTE(app, "/postern/service/restart")
+        .methods("POST"_method)([&](const crow::request& req, crow::response& res) {
+            auto body = crow::json::load(req.body);
+            if (!body || !body.has("pwd") || body["pwd"].s() != "CVTEMH") {
+                return FailResponse(res, ErrorCode::PARAMS_ERROR, "params error");
+            }
 
 #ifdef NDEBUG
-        // 重启应用
-        std::string restart = "sleep 0.01 && /etc/init.d/S99galaxy restart";
-        boost::process::child c(restart, boost::process::std_out > stdout, boost::process::std_err > stderr);
-        c.detach();
+            // 重启应用
+            std::string restart = "sleep 0.01 && /etc/init.d/S99galaxy restart";
+            boost::process::child c(restart, boost::process::std_out > stdout, boost::process::std_err > stderr);
+            c.detach();
 #else
-        std::cout << "Build type: Debug, System Not Restart" << std::endl;
+            std::cout << "Build type: Debug, System Not Restart" << std::endl;
 #endif
-        return SuccessResponse(res, "Restarting...");
-    });
+            return SuccessResponse(res, "Restarting...");
+        });
 }
