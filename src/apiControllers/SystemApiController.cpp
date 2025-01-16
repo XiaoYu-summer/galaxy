edefcf666c10b5a9419b2366eb9b19043750983f
xiayuqiang <xiayuqinag@cvte.com>
2025-01-16 09:54:53 +0800

    feat: add log-download interface

--- src/apiControllers/SystemApiController.cpp
+++ src/apiControllers/SystemApiController.cpp
@@ -1,12 +1,23 @@
+// #include <boost/filesystem.hpp>
 #include "Version.h"
 #include "apiControllers/SystemApiController.h"
+#include "utils/FileUtils.h"
+#include "utils/LogUtils.h"
+#include "utils/ResUtils.h"
 
 void SystemApiController::InitRoutes(CrowApp& crowApp) {
     CROW_ROUTE(crowApp, "/version").methods("GET"_method)([&] {
         crow::json::wvalue versionInfo(
-            {{"version", VersionInfo::VERSION}, {"hash", VersionInfo::GIT_HASH}, {"count", VersionInfo::COMMIT_COUNT}});
+            {{"version", VersionInfo::VERSION}, {"hash", VersionInfo::GIT_HASH}, {"count", VersionInfo::COMMIT_COUNT}}
+        );
         return versionInfo;
     });
 
-    CROW_ROUTE(crowApp, "/health").methods("GET"_method)([&] { return "OK"; });
+    CROW_ROUTE(crowApp, "/health").methods("GET"_method)([] { return "OK"; });
+    
+    CROW_ROUTE(crowApp, "/log-download").methods("GET"_method)([] (const crow::request& request, crow::response& response){ 
+        HandleFileRetrievalReq(response, LOG_FILE_ABSOLUTE_PATH, [](crow::response &response, const Poco::File &logFile) {
+            return ResponseWithFile(response, "log.txt", logFile);
+        });
+    });
 }
