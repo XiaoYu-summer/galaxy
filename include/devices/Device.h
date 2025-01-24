aba6f3c052e87efc81097f0543f9803b607f60bb
xiayuqiang <xiayuqinag@cvte.com>
2025-01-24 15:22:27 +0800

    refactor: delete DeviceAgent class

--- include/devices/Device.h
+++ include/devices/Device.h
@@ -1,73 +1,75 @@
 #pragma once
 #include <string>
 #include <memory>
-#include "devices/DeviceParams.h"
-
 
 class DeviceController;
+class DeviceNetworkInfo;
+class DeviceInfo;
 
 class Device
 {
+private:
+    Device(const DeviceNetworkInfo& info);
+    Device(DeviceController* controller);
+
 public:
     static std::shared_ptr<Device> CreateDevice(const DeviceNetworkInfo& info, DeviceController* controller = nullptr);
 
     Device() = default;
-    virtual ~Device() = default;
+    ~Device() = default;
     /**
      * 获取设备信息
      * @param info [out] 设备信息
      * @return true: 获取成功
      * */ 
-    virtual bool GetInfo(DeviceInfo&) const = 0;
+    bool GetInfo(DeviceInfo&) const;
     /**
      * 获取设备ID
      * @return 设备ID
      * */
-    virtual std::string GetId() const = 0;
-    /**
-     * 重置设备
-     * @return true: 重置成功
-     * */ 
-    virtual bool Reset() = 0;
+    std::string GetId() const;
     /**
      * 锁定设备
      * @param lock 是否锁定
      * @return true: 执行成功
      *  */    
-    virtual bool SetLock(bool lock) = 0;
+    bool SetLock(bool lock);
     /**
      * 设置音量
      * @param volume 音量值
      * @return true: 设置成功
      *  */
-    virtual bool SetVolume(uint16_t volume) = 0;
+    bool SetVolume(uint16_t volume);
     /**
      * 设备指示(设备寻址)
      * @return true: 寻址成功
     */
-    virtual bool Flashing() = 0;
+    bool Flashing();
     /**
      * 设备扬声器检查
      * @return true: 检查成功
     */
-    virtual bool CheckSpeaker() = 0;
+    bool CheckSpeaker();
 
     /**
      * 设备断开连接
      * @return true: 连接成功
     */
-    virtual bool Disconnect() = 0;
+    bool Disconnect();
 
     /**
      * 设备是否连接
      * @return true: 连接成功
     */
-    virtual bool IsConnecting() = 0;
+    bool IsConnecting();
 
     /**
      * 设备静音
      * @param mute 是否静音
      * @return true: 执行成功
     */
-    virtual bool SetMute(bool mute) = 0;
+    bool SetMute(bool mute);
+
+private:
+    std::shared_ptr<DeviceController> controller_;
 };
--- src/devices/Device.cpp
+++ src/devices/Device.cpp
@@ -1,6 +1,70 @@
-#include "devices/DeviceAgent.h"
+#include "devices/DeviceController.h"
+#include "devices/Device.h"
+#include "devices/DeviceParams.h"
+
+Device::Device(const DeviceNetworkInfo& info)
+    : controller_(DeviceController::CreateDeviceController(info))
+{}
+
+Device::Device(DeviceController* controller)
+    : controller_(controller)
+{}
 
 std::shared_ptr<Device> Device::CreateDevice(const DeviceNetworkInfo& info, DeviceController* controller)
 {
-    return std::make_shared<DeviceAgent>(info, controller);
+    std::shared_ptr<Device> device;
+    if (controller)
+        device.reset(new Device(controller));
+    else
+        device.reset(new Device(info));
+    return device;
+}
+
+bool Device::GetInfo(DeviceInfo& info) const
+{
+    return false;
+}
+
+std::string Device::GetId() const
+{
+    DeviceInfo info_;
+    if (!GetInfo(info_)){
+        return "";
+    }
+    return info_.id;
+}
+
+bool Device::CheckSpeaker()
+{
+    return false;
+}
+
+bool Device::SetLock(bool lock)
+{
+    return false;
+}
+
+bool Device::SetVolume(const uint16_t volume)
+{
+    return false;   
+}
+
+bool Device::Flashing()
+{
+    return false;
+}
+
+bool Device::Disconnect()
+{
+    return false;
+}
+
+bool Device::IsConnecting()
+{
+    return false;
+}
+
+bool Device::SetMute(bool mute)
+{
+    return false;
 }
