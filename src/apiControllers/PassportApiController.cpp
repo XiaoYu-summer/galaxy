5e5e067ed9a1e45f3b43eb9ba4828e8578c652be
xiayuqiang <xiayuqinag@cvte.com>
2025-01-24 11:10:27 +0800

    refactor: update token api

--- src/apiControllers/PassportApiController.cpp
+++ src/apiControllers/PassportApiController.cpp
@@ -14,67 +14,55 @@ void PassportApiController::InitRoutes(CrowApp& app) {
 }
 
 void PassportApiController::InitAdminRoutes(CrowApp& app){
-    CROW_ROUTE(app, "/passport/api/v1/token")
+    CROW_ROUTE(app, "/passport/api/v1/admin/token/generate")
         .methods("POST"_method)([&app](const crow::request& request, crow::response& response) {
-            auto requestBody = crow::json::load(request.body);
-            if (!requestBody) {
-                return FailResponse(response, ErrorCode::PASSWORD_ERROR, "Invalid request body");
-            }
+            HandleJsonReqWithParams<std::string, std::string>(request, response,
+                [&app, &request](const std::string& accountName, const std::string& password,crow::response& response) {
+                    const std::string accountFilePath = PassportUtils::GetAccountPasswordFilePath().string();
 
-            if (!requestBody.has("account") || !requestBody.has("password")) {
-                return FailResponse(response, ErrorCode::PARAMS_ERROR, "Invalid parameters");
-            }
+                    std::ifstream inFile(accountFilePath);
+                    if (!inFile) {
+                        PassportUtils::InitAccountPasswordFile();
+                        return FailResponse(response, ErrorCode::AUTH_ERROR, "Server Passport not found");
+                    }
 
-            std::string accountName = requestBody["account"].s();
-            std::string password = requestBody["password"].s();
-            std::string accountFilePath = PassportUtils::GetAccountPasswordFilePath().string();
+                    std::string fileContent((std::istreambuf_iterator<char>(inFile)), std::istreambuf_iterator<char>());
+                    inFile.close();
 
-            std::ifstream inFile(accountFilePath);
-            if (!inFile) {
-                PassportUtils::InitAccountPasswordFile();
-                return FailResponse(response, ErrorCode::AUTH_ERROR, "Server Passport not found");
-            }
+                    crow::json::rvalue accountPassword = crow::json::load(fileContent);
+                    if (accountName != accountPassword["account"].s() || password != accountPassword["password"].s()) {
+                        return FailResponse(response, ErrorCode::PASSWORD_ERROR, "Authorization failed");
+                    }
 
-            std::string fileContent((std::istreambuf_iterator<char>(inFile)), std::istreambuf_iterator<char>());
-            inFile.close();
+                    auto& session = app.get_context<Session>(request);
+                    boost::uuids::uuid uuid = boost::uuids::random_generator()();
+                    std::string token = boost::uuids::to_string(uuid);
 
-            crow::json::rvalue accountPassword = crow::json::load(fileContent);
-            if (accountName != accountPassword["account"].s() || password != accountPassword["password"].s()) {
-                return FailResponse(response, ErrorCode::PASSWORD_ERROR, "Authorization failed");
-            }
+                    session.set(TOKEN_KEY, token);
+                    auto expireTime = std::chrono::system_clock::now() + std::chrono::hours(1);
+                    session.set(TOKEN_EXPIRE_KEY, std::chrono::system_clock::to_time_t(expireTime));
 
-            auto& session = app.get_context<Session>(request);
-            boost::uuids::uuid uuid = boost::uuids::random_generator()();
-            std::string token = boost::uuids::to_string(uuid);
-
-            session.set(TOKEN_KEY, token);
-            auto expireTime = std::chrono::system_clock::now() + std::chrono::hours(1);
-            session.set(TOKEN_EXPIRE_KEY, std::chrono::system_clock::to_time_t(expireTime));
-
-            crow::json::wvalue responseData({{"accessToken", token}});
-            return SuccessResponse(response, "Gain token success", responseData);
+                    crow::json::wvalue responseData({{"accessToken", token}});
+                    return SuccessResponse(response, "Gain token success", responseData);
+                }, "account", "password");
         });
 
-    CROW_ROUTE(app, "/passport/api/v1/password")
+    CROW_ROUTE(app, "/passport/api/v1/admin/token/change")
         .methods("PUT"_method)([&app](const crow::request& request, crow::response& response) {
-            auto requestBody = crow::json::load(request.body);
-            if (!requestBody) {
-                return FailResponse(response, ErrorCode::PARAMS_ERROR, "Invalid request body");
-            }
+            HandleJsonReqWithParams<std::string, std::string>(request, response,
+                [&app, &request](const std::string& oldPassword, const std::string& newPassword,crow::response& response){
+                    auto [accountName, password] = PassportUtils::ReadAccountPasswordFromFile();
 
-            std::string oldPassword = requestBody["oldPassword"].s();
-            std::string newPassword = requestBody["newPassword"].s();
-            auto [accountName, password] = PassportUtils::ReadAccountPasswordFromFile();
+                    if (oldPassword != password) {
+                        return FailResponse(response, ErrorCode::OLD_PASSWORD_ERROR, "Old password is incorrect");
+                    }
 
-            if (oldPassword != password) {
-                return FailResponse(response, ErrorCode::OLD_PASSWORD_ERROR, "Old password is incorrect");
-            }
-
-            PassportUtils::WriteAccountPasswordToFile(accountName, newPassword);
-            return SuccessResponse(response, "Password changed successfully");
+                    PassportUtils::WriteAccountPasswordToFile(accountName, newPassword);
+                    return SuccessResponse(response, "Password changed successfully");
+                }, "oldPassword", "newPassword");
         });
 
-    CROW_ROUTE(app, "/passport/api/v1/token")
+    CROW_ROUTE(app, "/passport/api/v1/admin/token")
         .methods("DELETE"_method)([&app](const crow::request& request, crow::response& response) {
             auto& session = app.get_context<Session>(request);
             session.remove("token");
